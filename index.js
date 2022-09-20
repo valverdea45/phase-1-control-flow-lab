6e3ad54b8d7e709088ebdefee466e4b1b03624d7
Adrian <adrianvalverde45@gmail.com>
2022-09-20 12:32:41 -0400

    commit tree fde5696079d72adbb5c827c832be38f6f9c58444

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  if (someValue <= 400){
+    return `This one is on me!`
+  } else if (someValue <= 2500) {
+    return `I will gladly take your thirty bucks.`
+  } else if (someValue > 2500) {
+    return `No can do.`
+  }
+}
+function ternaryCheckCity(city){
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(money){
+  switch(money){
+    case `generous`: 
+      return `Thank you so much.`
+    case `not as generous`:
+      return `Thank you.`
+    default:
+      return `Bye.`
+  }
 }
